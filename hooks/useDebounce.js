ec2b6896f0a85aa97ddbd97b1cc477aa1e1c88bf
Yarin ordman <Yarinordman@gmail.com>
2022-04-04 10:26:54 +0300

    Yarin - useDebounce

--- hooks/useDebounce.js
+++ hooks/useDebounce.js
@@ -1,4 +1,24 @@
-// hook for
-export const useDebounce = ()=>{
+import { useEffect, useRef } from "react";
+/**
+ * @param  {Function} callback
+ * @param  {int} delay in ms default is 300ms
+ * return debounced function
+ */
+export const useDebounce = (callback, delay = 300) => {
+  const latestCallback = useRef();
+  const latestTimeout = useRef();
 
-}
+  useEffect(() => {
+    latestCallback.current = callback;
+  }, [callback]);
+
+  return () => {
+    if (latestTimeout.current) {
+      clearTimeout(latestTimeout.current);
+    }
+
+    latestTimeout.current = setTimeout(() => {
+      latestCallback.current();
+    }, delay);
+  };
+};
